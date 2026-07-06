0429008f33f01134566773b672cd29fe2eadbade
jypwang12 <jypwang12@gmail.com>
2026-07-06 11:29:57 -0400

    updated

--- run_with_csv.py
+++ run_with_csv.py
@@ -4,8 +4,7 @@ import sys
 
 csv = sys.argv[1]
 dp = DataProcessor(csv_path=csv)
-df = dp.load() 
+df = dp.load()
 
 print(df)
 print("\nSummary:\n", dp.summary())
-print("\nCLV (simple, churn=0.2):", dp.compute_clv(arpu_column="arpu", churn_rate=0.2))
